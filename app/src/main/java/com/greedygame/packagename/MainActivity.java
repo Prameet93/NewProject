67c8c5f72fbaedcdcf77790e29dc74439d0a51ea
Prameet31 <prameet31o@gmail.com>
2017-02-16 19:34:36 +0530

    showing title with app icon

--- app/src/main/java/com/greedygame/packagename/MainActivity.java
+++ app/src/main/java/com/greedygame/packagename/MainActivity.java
@@ -5,20 +5,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayAdapter<String> adapter;
     Context context;
+    private ArrayAdapter<String> adapter;
     ArrayList<String> innerArray = new ArrayList<String>();
     ArrayList<String> namesArray = new ArrayList<String>();
     ArrayList<String> newArray = new ArrayList<String>();
@@ -29,19 +34,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview);
+        adapter = new MyAdapter(this,innerArray);
+        listView.setAdapter(adapter);
+
         AsyncTaskRunner task = new AsyncTaskRunner();
         task.execute();
         context = this;
-        listView = (ListView) findViewById(R.id.listview);
-        adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, innerArray);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Construct the data source
+// Create the adapter to convert the array to views
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                String packageName = newArray.get(position);
+                String packageName = innerArray.get(position);
                 Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packageName);
                 if (launchIntent != null) {
                     startActivity(launchIntent);//null pointer check in case package name was not found
@@ -50,6 +58,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public class MyAdapter extends ArrayAdapter<String> {
+        public MyAdapter(Context context, ArrayList<String> objects) {
+            super(context, 0, objects);
+        }
+
+        @Override
+        public int getCount() {
+            return innerArray.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_view_items, null);
+            TextView textView = (TextView) convertView.findViewById(R.id.textView);
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+            String packageName = innerArray.get(position);
+            PackageManager packageManager= getApplicationContext().getPackageManager();
+            try {
+                String appName = (String) packageManager.getApplicationLabel(packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA));
+                textView.setText(appName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            Drawable icon = null;
+            try {
+                icon = getPackageManager().getApplicationIcon(packageName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            imageView.setImageDrawable(icon);
+            return convertView;
+
+        }
+
+    }
     public class AsyncTaskRunner extends AsyncTask<String, String, String> {
         ProgressDialog pd = new ProgressDialog(MainActivity.this);
 
@@ -75,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            innerArray.addAll(namesArray);
+            innerArray.addAll(newArray);
             adapter.notifyDataSetChanged();
             pd.dismiss();
         }
