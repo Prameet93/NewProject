e2abfe7ca62e705e2358b6c304c66ab39d2c88c4
Prameet31 <prameet31o@gmail.com>
2017-02-21 19:33:31 +0530

    Added Search bar

--- app/src/main/java/com/greedygame/packagename/MainActivity.java
+++ app/src/main/java/com/greedygame/packagename/MainActivity.java
@@ -9,11 +9,16 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -23,21 +28,39 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
-    private ArrayAdapter<String> adapter;
+    private MyAdapter adapter;
     ArrayList<String> innerArray = new ArrayList<String>();
     ArrayList<String> namesArray = new ArrayList<String>();
     ArrayList<String> newArray = new ArrayList<String>();
     ListView listView;
+    EditText editText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         listView = (ListView) findViewById(R.id.listview);
-        adapter = new MyAdapter(this,innerArray);
+        editText = (EditText) findViewById(R.id.txtseearch);
+        adapter = new MyAdapter(this, innerArray);
         listView.setAdapter(adapter);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence cs, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence cs, int i, int i1, int i2) {
+                adapter.getFilter().filter(cs.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         AsyncTaskRunner task = new AsyncTaskRunner();
         task.execute();
@@ -49,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                String packageName = innerArray.get(position);
+                String packageName = adapter.getTempArray().get(position);
                 Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packageName);
                 if (launchIntent != null) {
                     startActivity(launchIntent);//null pointer check in case package name was not found
@@ -58,25 +81,44 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public class MyAdapter extends ArrayAdapter<String> {
+    public class MyAdapter extends BaseAdapter implements Filterable {
+        Context mContext;
+        ArrayList<String> tempArray = new ArrayList();
+
+
         public MyAdapter(Context context, ArrayList<String> objects) {
-            super(context, 0, objects);
+            mContext = context;
+            tempArray = objects;
+        }
+
+        public ArrayList<String> getTempArray() {
+            return tempArray;
         }
 
         @Override
         public int getCount() {
-            return innerArray.size();
+            return tempArray.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.list_view_items, null);
             TextView textView = (TextView) convertView.findViewById(R.id.textView);
             ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
-            String packageName = innerArray.get(position);
-            PackageManager packageManager= getApplicationContext().getPackageManager();
+            String packageName = tempArray.get(position);
+            PackageManager packageManager = getApplicationContext().getPackageManager();
             try {
                 String appName = (String) packageManager.getApplicationLabel(packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA));
                 textView.setText(appName);
@@ -91,10 +133,40 @@ public class MainActivity extends AppCompatActivity {
             }
             imageView.setImageDrawable(icon);
             return convertView;
+        }
 
+        @Override
+        public Filter getFilter() {
+            Filter filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence cs) {
+                    FilterResults results = new FilterResults();
+                    List<String> FilteredArrList = new ArrayList<String>();
+                    for (int i = 0; i < namesArray.size(); i++) {
+                        String data = namesArray.get(i);
+                        if (data.toLowerCase().startsWith(cs.toString().toLowerCase())) {
+                            FilteredArrList.add(innerArray.get(i));
+                        }
+                    }
+
+                    results.count = FilteredArrList.size();
+                    results.values = FilteredArrList;
+
+
+                    return results;
+                }
+
+                @Override
+                protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                    tempArray = (ArrayList<String>) filterResults.values;
+                    notifyDataSetChanged();
+                }
+            };
+            return filter;
         }
 
     }
+
     public class AsyncTaskRunner extends AsyncTask<String, String, String> {
         ProgressDialog pd = new ProgressDialog(MainActivity.this);
 
@@ -102,16 +174,11 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... params) {
             PackageManager pm = getPackageManager();
             List<ApplicationInfo> apps = pm.getInstalledApplications(0);
-            for(int i=0;i<apps.size();i++){
+            for (int i = 0; i < apps.size(); i++) {
                 newArray.add(apps.get(i).packageName);
                 String applicationName = (String) (apps.get(i) != null ? pm.getApplicationLabel(apps.get(i)) : "(unknown)");
                 //namesArray.add(apps.get(i).packageName);
                 namesArray.add(applicationName);
-            }
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
 
             return null;
